9f7a8dfe10ba10c5ed96ad5e4d1c4a3777f1cb56
Mateusz Grzonka <alpinus4@gmail.com>
2020-10-14 15:56:46 +0200

    Refactor Taxi.gd

--- Void/Taxi.gd
+++ Void/Taxi.gd
@@ -1,41 +1,55 @@
 extends KinematicBody2D
 
-export (int) var speed = 0 #initial speed
-export (float) var rotation_speed = 1.1 #rotation factor
+const INITIAL_SPEED = 0
+const ROTATION_SPEED = 1.1 #rotation factor
+const MINIMAL_SPEED_TO_ROTATE = 55
+const MAX_FORWARD_SPEED = 440
+const MAX_BACKWARDS_SPEED = 220
+const FORWARD_ACCELERATION = 4
+const BACKWARDS_ACCELERATION = 2
+const SLOWING_FACTOR_WHEN_MOVING_FORWARD = 4
+const SLOWIG_FACTOR_WHEN_MOVING_BACKWARDS = 2
 
-var velocity = Vector2()
-var rotation_dir #rotation direction
+var speed = INITIAL_SPEED
+var velocity: Vector2
+var rotation_dir
 
 func get_input():
 	rotation_dir = 0 #initial rotation direction
-	velocity = Vector2()
-	if Input.is_action_pressed('ui_right'): #right
-		if(speed>55 or speed<-55): #rotation depending on the speed, so you can't turn in place
-			rotation_dir += 1 #rotation direction
-	if Input.is_action_pressed('ui_left'): #left
-		if(speed>75 or speed<-75): #rotation depending on the speed, so you can't turn in place
-			rotation_dir -= 1 #rotation direction
+	velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed('ui_right'):
+		if(abs(speed) > MINIMAL_SPEED_TO_ROTATE): #rotation depending on the speed, so you can't turn in place
+			rotation_dir += 1
+	
+	if Input.is_action_pressed('ui_left'):
+		if(abs(speed) > MINIMAL_SPEED_TO_ROTATE): #rotation depending on the speed, so you can't turn in place
+			rotation_dir -= 1 
+	
 	if Input.is_action_pressed('ui_up'): #forward
-		if (speed < 440): #maximum speed forward
-			speed=speed+4 #forward acceleration
+		if (speed < MAX_FORWARD_SPEED):
+			speed += FORWARD_ACCELERATION
 		velocity = Vector2(speed, 0).rotated(rotation)
-	else: speed=speed-4 #don't delete
+	else: speed -= FORWARD_ACCELERATION #don't delete
+	
 	if Input.is_action_pressed('ui_down'): #backwards
-		if (speed > -220): #maximum speed backwards
-			speed=speed-2 #backwards acceleration
-			if(speed>0):
-				speed=speed-32 #brakes
+		if (speed > -MAX_BACKWARDS_SPEED):
+			speed -= BACKWARDS_ACCELERATION #backwards acceleration
+			if(speed > 0):
+				speed -= 32 #brakes
 		velocity = Vector2(speed, 0).rotated(rotation)
-	else: speed=speed+4 #don't delete
+	else: speed += FORWARD_ACCELERATION #don't delete
+	
 	if InputDefault: #continuous drive at the last registered speed after no user input
 		velocity = Vector2(speed, 0).rotated(rotation) 
-	if Input.is_action_pressed('ui_up')==false and Input.is_action_pressed('ui_down')==false: #slowing down after no user input
+	
+	if Input.is_action_pressed('ui_up') == false and Input.is_action_pressed('ui_down') == false: #slowing down after no user input
 		if (speed > 0):
-			speed=speed-4
+			speed -= SLOWING_FACTOR_WHEN_MOVING_FORWARD
 		elif (speed < 0):
-			speed=speed+2
+			speed += SLOWIG_FACTOR_WHEN_MOVING_BACKWARDS
 
 func _physics_process(delta):
 	get_input()
-	rotation += rotation_dir * rotation_speed * delta
+	rotation += rotation_dir * ROTATION_SPEED * delta
 	velocity = move_and_slide(velocity)
