0e87ad1320473d142d99fff21a85dfc17d5f7ccf
Mateusz Grzonka <alpinus4@gmail.com>
2020-10-20 18:21:46 +0200

    Improve movement

--- Void/Taxi.gd
+++ Void/Taxi.gd
@@ -5,21 +5,26 @@ const ROTATION_FACTOR = 1.1
 const MIN_SPEED_TO_ROTATE = 55
 const MAX_FORWARD_SPEED = 4400
 const MAX_BACKWARDS_SPEED = 2200
-const FORWARD_ACCELERATION = 4
-const BACKWARDS_ACCELERATION = 2
-const SLOWING_FACTOR_WHEN_MOVING_FORWARD = 4
-const SLOWING_FACTOR_WHEN_MOVING_BACKWARDS = 2
+const FORWARD_ACCELERATION_WEIGHT = 0.004
+const BACKWARDS_ACCELERATION_WEIGHT = 0.0031
+const SLOW_WEIGHT_WHEN_MOVING_FORWARD = 0.003
+const SLOW_WEIGHT_WHEN_MOVING_BACKWARDS = 0.003
 const BOUNCE_FACTOR = 0.5
 const DESTRUCTION_FACTOR = 75
 
 var speed = INITIAL_SPEED
+
 var velocity: Vector2
 var rotation_dir
 var collision
+
 var x = 0
 var money = 0
 var random_money
 var health = 100
+
+onready var collision_raycast_forward = get_node("RayCast2DForward")
+onready var collision_raycast_backwards = get_node("RayCast2DBackwards")
 
 func get_input():
 	rotation_dir = 0 #initial rotation direction
@@ -34,34 +39,29 @@ func get_input():
 			rotation_dir -= 1 
 	
 	if Input.is_action_pressed('ui_up'): #forward
-		if (speed < MAX_FORWARD_SPEED):
-			speed += FORWARD_ACCELERATION
-		if(speed < 0):
-			speed += 32 #brakes
-		velocity = Vector2(speed, 0).rotated(rotation)
+		if not collision_raycast_forward.is_colliding():
+			speed = lerp(speed, MAX_FORWARD_SPEED, FORWARD_ACCELERATION_WEIGHT)
+			velocity = Vector2(speed, 0).rotated(rotation)
 	
 	if Input.is_action_pressed('ui_down'): #backwards
-		if (speed > -MAX_BACKWARDS_SPEED):
-			speed -= BACKWARDS_ACCELERATION #backwards acceleration
-			if(speed > 0):
-				speed -= 32 #brakes
-		velocity = Vector2(speed, 0).rotated(rotation)
+		if not collision_raycast_backwards.is_colliding():
+			speed = lerp(speed, -MAX_BACKWARDS_SPEED, BACKWARDS_ACCELERATION_WEIGHT)
+			velocity = Vector2(speed, 0).rotated(rotation)
 	
 	if Input.is_action_pressed('ui_up') == false and Input.is_action_pressed('ui_down') == false: #slowing down after no user input
 		if (speed > 0):
-			speed -= SLOWING_FACTOR_WHEN_MOVING_FORWARD
+			speed = lerp(speed, 0, SLOW_WEIGHT_WHEN_MOVING_FORWARD)
 		elif (speed < 0):
-			speed += SLOWING_FACTOR_WHEN_MOVING_BACKWARDS
+			speed = lerp(speed, 0, SLOW_WEIGHT_WHEN_MOVING_BACKWARDS)
 	
 	if Input.is_action_pressed('ui_cancel'):
 		get_tree().quit()
 	
 	if collision: #bounce mechanic
-		if(speed>33):
-			health=int(health-speed/DESTRUCTION_FACTOR)
-		if(speed<-33):
-			health=int(health+speed/DESTRUCTION_FACTOR)
-		speed = int(-speed * BOUNCE_FACTOR)
+		if abs(speed) > 50:
+			health=int(health-abs(speed)/DESTRUCTION_FACTOR)
+		if abs(speed) > 30:
+			speed = int(-speed * BOUNCE_FACTOR)
 	
 	velocity = Vector2(speed, 0).rotated(rotation)
 
@@ -71,10 +71,10 @@ func _physics_process(delta):
 	collision = move_and_collide(velocity * delta) #registering collisions for bounce mechanic
 	$CanvasLayer/VBoxContainer/Health.text = "Health: " + var2str(health) + " HP" #current health
 	$CanvasLayer/VBoxContainer/Money.text = "Money: " + var2str(money) + " $" #current money
-	if(speed > 0): #current speed
-		$CanvasLayer/VBoxContainer/Speed.text = "Speed: " + var2str(speed) + " footballfields/s"
-	if(speed < 0): #current speed
-		$CanvasLayer/VBoxContainer/Speed.text = "Speed: " + var2str(-speed) + " footballfields/s"
+	if collision_raycast_forward.is_colliding() || collision_raycast_backwards.is_colliding():
+		$CanvasLayer/VBoxContainer/Speed.text = "Speed: " + var2str(0) + " footballfields/s"
+	else:
+		$CanvasLayer/VBoxContainer/Speed.text = "Speed: " + var2str(abs(speed)) + " footballfields/s"
 	if(health<=0): #game over
 		get_tree().change_scene("res://MainMenu.tscn")
 
