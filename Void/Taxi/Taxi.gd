9dc71149f4656269ec91142d51c0b449286af50c
Mateusz Grzonka <alpinus4@gmail.com>
2020-10-21 14:16:20 +0200

    Change default speed unit to footballfiels/s

--- Void/Taxi/Taxi.gd
+++ Void/Taxi/Taxi.gd
@@ -81,9 +81,9 @@ func taxi_UI():
 	$CanvasLayer/HBoxContainer/VBoxContainer/Health.text = "Health: " + var2str(health) + " HP" #current health
 	$CanvasLayer/HBoxContainer/VBoxContainer2/Money.text = "Money: " + var2str(money) + " $" #current money
 	if collision_raycast_forward.is_colliding() || collision_raycast_backwards.is_colliding():
-		$CanvasLayer/HBoxContainer/VBoxContainer/Speed.text = "Speed: 0 % c"
+		$CanvasLayer/HBoxContainer/VBoxContainer/Speed.text = "Speed: 0 footballfields/s"
 	else:
-		$CanvasLayer/HBoxContainer/VBoxContainer/Speed.text = "Speed: " + var2str(abs(speed/100)) + " % c"
+		$CanvasLayer/HBoxContainer/VBoxContainer/Speed.text = "Speed: " + var2str(abs(speed)) + " footballfields/s"
 
 func emit_pickup_signal(planet):
 	emit_signal("passenger_pickup", planet)
