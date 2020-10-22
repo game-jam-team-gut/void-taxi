a62a02a397528d63b0ca0014ddbe3079ee87c738
filip-szweda <filip.szweda1@gmail.com>
2020-10-22 22:22:06 +0200

    Credits

--- MainMenu/MainMenu.gd
+++ MainMenu/MainMenu.gd
@@ -3,6 +3,8 @@ extends Control
 func _on_Button_pressed():
 	get_tree().change_scene("res://Main.tscn")
 
+func _on_CreditsButton_pressed():
+	get_tree().change_scene("res://Credits.tscn")
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
