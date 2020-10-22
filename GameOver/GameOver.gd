fed6e05858d6c23c840d4fb3e6b0c874ec1fc146
Mateusz Grzonka <alpinus4@gmail.com>
2020-10-22 20:43:11 +0000

    MainMenu, Credits and GameOver scenes improvements

--- GameOver/GameOver.gd
+++ GameOver/GameOver.gd
@@ -1,10 +1,10 @@
 extends Control
 
-
-
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://Main.tscn")
 
+func _on_Button_pressed():
+	get_tree().change_scene("res://MainMenu.tscn")
 
 func _on_Quit_pressed():
 	get_tree().quit()
